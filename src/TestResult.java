cd8987bcbd78956fb3d12a7e55ea2febbe29bc91
KAZAHv2 <91937513+KAZAHv2@users.noreply.github.com>
2022-02-06 15:37:10 +0200

    Update TestResult.java

--- src/TestResult.java
+++ src/TestResult.java
@@ -2,11 +2,23 @@
 
 //import domain.Exercise;
 
+import java.util.Scanner;
+
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        System.out.print("Введіть ваше число долі: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        while (s.length() > 1) {
+            int sum = 0;
+            for (char c : s.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    sum += (c - '0');
+                }
+            }
+            s = String.valueOf(sum);
+        }
+        System.out.println("Ваше число долі: " + s);
     }
 }
